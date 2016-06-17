6f7cc62eddc1dbc00711838ad587f72b4b952df3
Ari Kallio <westernak@users.noreply.github.com>
2016-06-17 09:04:44 +0300

    Review
    Luokan nimi korjattu

--- REST/src/java/fi/Hello.java
+++ REST/src/java/fi/Hello.java
@@ -5,7 +5,7 @@ import java.util.Date;
 /**
  * @author ak
  */
-public class DateAfterValidator implements Validator {
+public class Hello {
 
   public static void main(String[] args) {
     System.out.println(new Date());
