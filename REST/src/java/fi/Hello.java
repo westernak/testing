278c9ef4e6eb8a473a5aeca8b9553478b66f927f
Ari Kallio <ari.kallio@western.fi>
2016-06-17 09:01:53 +0300

    Hakemistotesti
    Lisätty esimerkki tiedostoja hakemistorakenteen kera.

---
+++ REST/src/java/fi/Hello.java
@@ -0,0 +1,14 @@
+package fi;
+
+import java.util.Date;
+
+/**
+ * @author ak
+ */
+public class DateAfterValidator implements Validator {
+
+  public static void main(String[] args) {
+    System.out.println(new Date());
+  }
+
+}
